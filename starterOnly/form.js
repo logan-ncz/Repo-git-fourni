6f97aecda59d50ac1be38616fdcc803138fc98cc
Logan Nochez <logannochez@hotmail.fr>
2021-05-29 01:21:59 +0200

    2ème issue

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,9 +1,60 @@
-// form validation
+// DOM elements
+
+const errorAlert = document.querySelectorAll(".message-alerte");
+
+
+// inputs validations
+
 function firstValidation() {
-    let inputValue = document.getElementById("first").value;
-    if (inputValue !== null && inputValue.length > 2) {
-      return true;
-    }  else {
-      return false;
-    }
+	let inputValue = document.getElementById("first").value;
+	if (inputValue !== null && inputValue.length > 2) {
+    errorAlert[0].style.display = "none";
+    return true;
+  }  else { 
+    errorAlert[0].style.display = "block";
+    return false;
   }
+}
+
+function lastValidation() {
+	let inputValue = document.getElementById("last").value;
+	if (inputValue !== null && inputValue.length > 2) {
+    errorAlert[0].style.display = "none";
+    return true;
+  }  else { 
+    errorAlert[0].style.display = "block";
+    return false;
+  }
+}
+
+function emailValidation() {
+	let regex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
+	let inputValue = document.getElementById("email").value;
+	return regex.test(inputValue);
+}
+
+function quantityValidation() {
+	let regex = /^[0-9]+$/;
+	let inputValue = document.getElementById("quantity").value;
+	return regex.test(inputValue);
+}
+
+function locationValidation() {
+	let radioButtons = document.querySelectorAll(".checkbox-input[type=radio]");
+	for (let radio of radioButtons) {
+		if (radio.checked === true) return true;
+	}
+	return false;
+}
+
+function checkboxValidation() {
+	let inputValue = document.getElementById("checkbox1").checked;
+	return inputValue;
+}
+
+document
+	.getElementById("button")
+	.addEventListener("click", function formValidation(event) {
+		event.preventDefault();
+		console.log(checkboxValidation());
+	});
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,24 +8,58 @@ function editNav() {
 }
 
 // DOM Elements
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll(".close")
+const form = document.querySelector("form");
 
 // launch modal event
+
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
+
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// form validation functions
+
+function firstValidation() {
+	let inputValue = document.getElementById("first").value;
+	if (inputValue !== null && inputValue.length < 2) return true;
+	else return false;
+}
+
+function lastValidation() {
+	let inputValue = document.getElementById("last").value;
+}
+
+function emailValidation() {
+	let inputValue = document.getElementById("email").value;
+}
+
+function quantityValidation() {
+	let inputValue = document.getElementById("quantity").value;
+}
+function locationValidation() {}
+
+function checkboxValidation() {
+	let inputValue = document.getElementById("checkbox1").value;
+}
 
 // close modal event
+
 closeModalBtn.forEach((span) => span.addEventListener("click", closeModal));
 
+form[0].addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
 // close modal form
+
 function closeModal() {
   modalbg.style.display = "none";
 }
