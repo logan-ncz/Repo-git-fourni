8626f081768c91c11df8a9b5b36e3f88131fbad7
Logan Nochez <logannochez@hotmail.fr>
2021-05-29 01:50:48 +0200

    3ème issue

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,7 +13,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll(".close")
-const form = document.querySelector("form");
 
 // launch modal event
 
@@ -23,31 +22,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 function launchModal() {
   modalbg.style.display = "block";
-}
-
-// form validation functions
-
-function firstValidation() {
-	let inputValue = document.getElementById("first").value;
-	if (inputValue !== null && inputValue.length < 2) return true;
-	else return false;
-}
-
-function lastValidation() {
-	let inputValue = document.getElementById("last").value;
-}
-
-function emailValidation() {
-	let inputValue = document.getElementById("email").value;
-}
-
-function quantityValidation() {
-	let inputValue = document.getElementById("quantity").value;
-}
-function locationValidation() {}
-
-function checkboxValidation() {
-	let inputValue = document.getElementById("checkbox1").value;
 }
 
 // close modal event
